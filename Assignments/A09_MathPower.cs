b856ea24f221168ba049931b91fb9c07a5a8b5b6
Nicklas Damgren <AnorZaken@users.noreply.github.com>
2021-03-26 03:55:08 +0100

    A09

--- Assignments/A09_MathPower.cs
+++ Assignments/A09_MathPower.cs
@@ -1,16 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleAssignments.Assignments
 {
+    using static Math;
+
     record A09_MathPower() : Assignment(9, "Math Power")
     {
         protected override void Implementation()
         {
-            //ConsoleX.TryReadNumber
+            Console.Write($"Please enter a decimal number (e.g. {2.41}): "); // the example is good because it informs the user what the correct decimal symbol is.
+            if (!ConsoleX.TryReadManyAttempts(ConsoleX.TryReadNumber, out double number, prompt: ""))
+                return;
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine($" Sqrt({number}) = {Sqrt(number):#.##}");
+            Console.WriteLine($" Pow'2({number}) = {Pow(number, 2):#.##}");
+            Console.WriteLine($" Pow'10({number}) = {Pow(number, 10):n2}");
         }
     }
 }
